95ede63dae8a3cef1abb6d82c1113f752d594b76
Luis Valero <luisvaleroxx@gmail.com>
2022-03-31 00:42:32 +0200

    add initial project
    Generate base project with create-react-app

---
+++ web-twitter/src/App.js
@@ -0,0 +1,7 @@
+import { WebTwitter } from "./views/WebTwitter";
+
+function App() {
+	return <WebTwitter />;
+}
+
+export default App;
---
+++ web-twitter/src/views/WebTwitter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const WebTwitter = () => {
+	return <p>WebTwitter</p>;
+};
+
+WebTwitter.propTypes = {};
+
+export default WebTwitter;
